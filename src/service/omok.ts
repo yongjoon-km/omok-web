7265e558bb4c258a8c873b07d435f2c8099d2c11
yongjoon <penubokim@gmail.com>
2022-04-17 22:05:18 +0900

    Set unique Identifier for future state management
    To negotiate the state of omok game, I use unique user identifier (random hash) between web socket requests.

--- src/service/omok.ts
+++ src/service/omok.ts
@@ -2,6 +2,21 @@ import { dispatch } from '../action/gameStore'
 import { sendMessage } from './server'
 import { Message } from './type'
 
+const ID: string = generateId()
+
+const HASH_SIZE = 3
+
+function generateId(): string {
+  const characters = '0123456789ABCDEFGHIJKLMNPQRSTUVWXYZ'
+  let randomString = ''
+  for (let i = 0; i <= HASH_SIZE; i++) {
+    randomString += characters[Math.floor(Math.random() * characters.length)]
+  }
+  const id = randomString
+  console.log('id is', id)
+  return id
+}
+
 export function place(x: number, y: number) {
   const message = {
     type: 'place',

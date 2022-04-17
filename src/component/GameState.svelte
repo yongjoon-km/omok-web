29c4572161e88fc36c3f48c458143f25d7f47509
yongjoon <penubokim@gmail.com>
2022-04-17 11:01:28 +0900

    Polish code

--- src/component/GameState.svelte
+++ src/component/GameState.svelte
@@ -1,24 +1,21 @@
 <script lang="ts">
   import { state } from '../action/gameStore'
   import { Turn, Winner } from '../action/gameTypes'
-  let isGameOver: boolean
-  let winner: Winner = null
-  state.subscribe(({ isGameOver: isGameOverState, winner: newWinner }) => {
-    isGameOver = isGameOverState
-    winner = newWinner
-  })
 
   const getWinnerText = (winner: Winner) => {
-    if (winner === null) {
-      return 'No winner'
+    switch (winner) {
+      case Turn.Black:
+        return 'Black'
+      case Turn.White:
+        return 'White'
+      default:
+        return 'No winner'
     }
-    if (winner === Turn.Black) return 'Black'
-    return 'White'
   }
 </script>
 
-{#if isGameOver === true}
-  <div><p class="text-white">{getWinnerText(winner)} is won!!!</p></div>
+{#if $state.isGameOver === true}
+  <div><p class="text-white">{getWinnerText($state.winner)} is won!!!</p></div>
 {:else}
   <div><p class="text-white">game is playing</p></div>
 {/if}

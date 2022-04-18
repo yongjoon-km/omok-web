2b33e5894c1ddf1172c227d6b1ef5b14a95afb23
yongjoon <penubokim@gmail.com>
2022-04-18 23:15:48 +0900

    Add debug select component for changing user stone

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import Board from './board/Board.svelte'
   import GameState from './component/GameState.svelte'
   import GameControll from './component/GameControll.svelte'
+  import GameUserStoneDebugSelect from './component/GameUserStoneDebugSelect.svelte'
 </script>
 
 <main>
   <div class="flex flex-col justify-center items-center w-screen h-screen">
+    <GameUserStoneDebugSelect />
     <GameState />
     <Board />
     <GameControll />
---
+++ src/component/GameUserStoneDebugSelect.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Turn, State } from '../action/gameTypes'
+  import { state } from '../action/gameStore'
+
+  function change(state: State, stone: Turn): State {
+    console.log('change user stone to ', state, stone)
+    return { ...state, userStone: stone }
+  }
+
+  const handleChange = (e) => {
+    state.update((s) => change(s, +e.target.value as Turn))
+  }
+</script>
+
+<select class="select w-full max-w-xs bg-white" on:change={handleChange}>
+  <option value={Turn.Black}>Black</option>
+  <option value={Turn.White}>White</option>
+</select>

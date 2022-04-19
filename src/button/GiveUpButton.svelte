69e8def1d08af59e5b03a22ac6f640aa3454ecfb
yongjoon <penubokim@gmail.com>
2022-04-19 20:32:03 +0900

    Disable giveup button when other's turn

--- src/button/GiveUpButton.svelte
+++ src/button/GiveUpButton.svelte
@@ -1,7 +1,12 @@
 <script>
   import { giveup } from '../service/omok'
+  import { state } from '../action/gameStore'
 
   const handleClick = () => giveup()
 </script>
 
-<button on:click={handleClick} class="btn btn-outline btn-wide">Give Up</button>
+<button
+  on:click={handleClick}
+  disabled={$state.turn !== $state.userStone}
+  class="btn btn-outline btn-wide">Give Up</button
+>

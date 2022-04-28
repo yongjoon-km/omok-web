8e6d8a3923ad71e59bd35b5a13a58bb30268a3d2
yongjoon <penubokim@gmail.com>
2022-04-28 22:24:03 +0900

    Create latest stone indicator component

---
+++ src/board/CurrentStonePointer.svelte
@@ -0,0 +1,8 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  class="stroke-current flex-shrink-0"
+>
+  <circle cx="50%" cy="50%" stroke-width="0" fill="green" r="2" />
+</svg>

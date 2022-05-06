ebb81b1b6661b7e4307353b24f03c9543e959ef4
yongjoon <penubokim@gmail.com>
2022-05-06 13:42:16 +0900

    Remove unused button

--- src/button/StartButton.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import { Turn } from '../action/gameTypes.ts'
-  import { start } from '../service/omok'
-
-  const handleClick = () => start(Turn.Black)
-</script>
-
-<button on:click={handleClick} class="btn btn-outline btn-wide">Start</button>
